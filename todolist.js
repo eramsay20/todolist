9ec310d065f028e2145b3cddc09f8dec2a372472
Eric Ramsay <64610054+eramsay20@users.noreply.github.com>
2020-05-26 10:56:05 -0400

    initialize and build frame

---
+++ todolist.js
@@ -0,0 +1,19 @@
+// Requirements:
+    // 1. App should utilize JavaScript, HTML, and CSS
+    // 2. App should allow users to submit/create tasks
+    // 3. Tasks should be displayed in a list
+    // 4. List items should be removable (able to delete entries)
+
+// Stretch Requirements:
+    // 1. Button to select all list items
+    // 2. Description field for each item
+    // 3. Due Date field for each item
+    // 4. Make list items editable
+
+// Technical Requirements:
+    // ● All event listeners should be defined in “window.onload” with named functions
+    // ● No global variables
+    // ● All styling in CSS file
+    // ● All events and functionality in JavaScript file
+    // ● Multiple functions
+    // ● Comments in the code explaining each section
